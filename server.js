68fc4c3babac0d0e5e6ebbebc2fbcda334f5a84b
Kushagra Seth <kseth919@gmail.com>
2020-04-29 02:20:22 +0530

    Setting PORT as env variable port

--- server.js
+++ server.js
@@ -38,4 +38,5 @@ app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) });
 
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
-app.listen(3000, () => console.log("App is running on Port 3000"));
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`App is running on Port ${PORT}`));
